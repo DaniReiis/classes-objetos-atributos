13b4b682cb00ff36ecbb01f4f1dcc2f5413c3e76
DaniReiis <danielreis350@gmail.com>
2025-07-16 19:21:44 -0300

    Exercicios usando classes, objetos e atributos

---
+++ Operacoes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    internal class Operacoes
+    {
+
+        public double A;
+        public double B; 
+        public double C;
+
+        public double Perimeto(double A, double B, double C)
+        {
+            return (A + B + C) / 2;
+        }
+
+        public double Area(double A, double B, double C)
+        {
+            double p = Perimeto(A, B, C);
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+
+        public double MaiorArea(double areaX, double areaY)
+        {
+            if (areaX > areaY)
+            {
+                return areaX;
+            }
+            else
+            {
+                return areaY;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+using System;
+using classes;
+
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Operacoes x, y, maiorArea;
+            x = new Operacoes();
+            y = new Operacoes();
+            maiorArea = new Operacoes();
+
+            Console.WriteLine("Entre com as medidas do trinângulo X:");
+            x.A = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do trinângulo Y:");
+            y.A = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+
+            double xp = x.Perimeto(x.A, x.B, x.C);
+            double areaX = x.Area(x.A, x.B, x.C);
+
+            double yp = x.Perimeto(y.A, y.B, y.C);
+            double areaY = x.Area(y.A, y.B, y.C);
+
+            Console.WriteLine("Área de X: " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de y: " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+
+            Console.WriteLine("Maior área é " + maiorArea.MaiorArea(areaX, areaY).ToString("F4", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+        }
+    }
+}
